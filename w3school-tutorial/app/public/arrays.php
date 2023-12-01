bf51a2c677bde4b3ec27176a1c429b6eca790266
arifoglu <88225121+arifoglu@users.noreply.github.com>
2023-12-01 10:49:28 +0100

    i finished type of arrays

--- w3school-tutorial/app/public/arrays.php
+++ w3school-tutorial/app/public/arrays.php
@@ -9,8 +9,88 @@
 <body>
     <?php
 ////////////////////////////////////////////////////////Arrays 
+//////////////////////////////////////indexed arrays
+//1)create an array 
+$cars = array ("volvo","bmw","toyota");
+echo $cars[0]."<br>".$cars[1]."<br>".$cars[2];
+echo "<br>";
 
+//count()
+echo "this array count is " . count($cars);
+echo "<br>";
 
+//2)create an array
+$cars2 = array();
+$cars2[0] = "ferrari";
+$cars2[1] = "audi";
+$cars2[2] = "nissan";
+print_r($cars2);
+echo "<br>";
+
+for($x = 0; $x < count($cars2);$x++){
+    echo $cars[$x];
+    echo "<br>";
+}
+
+//////////////////////////////////////associative arrays
+//
+$ages = array("peter"=>"35","helen"=>"25","galen"=>"55");
+echo $ages["peter"];
+echo "<br>";
+
+foreach($ages as $x=>$x_value){ // <<<$x>>> is key ----- <<<$x_value>>> is value
+    echo $x ." ".$x_value ."<br>" ;
+}
+
+//////////////////////////////////////multidimensional arrays
+//
+$cars3 = array (
+    array("Volvo",22,18),
+    array("BMW",15,13),
+    array("Saab",5,2),
+    array("Land Rover",17,15)
+  );
+echo $cars3[0][0]."<br>";
+echo $cars3[1][0]."<br>";
+
+for ($row = 0; $row < 4; $row++) {
+    echo "<p><b>Row number $row</b></p>";
+    echo "<ul>";
+    for ($col = 0; $col < 3; $col++) {
+      echo "<li>".$cars3[$row][$col]."</li>";
+    }
+    echo "</ul>";
+  }
+echo "<br>";
+
+//////////////////////////////////////sorting arrays
+//sort() sort arrays in ascending order
+$cars4 = array("volvo","bmw","toyota");
+sort($cars4);
+print_r($cars4);
+echo "<br>";
+
+$numbers = array(4, 6, 2, 22, 11);
+sort($numbers);
+print_r($numbers);
+echo "<br>";
+
+//rsort() - sort arrays in descending order
+$cars5 = array("volvo","bmw","toyota");
+rsort($cars5);
+print_r($cars5);
+echo "<br>";
+
+//asort() -Sort Array (Ascending Order), According to Value 
+$ageN = array("Peter"=>"35", "Ben"=>"47", "Joe"=>"43");
+asort($ageN);
+print_r($ageN);
+echo "<br>";
+
+//ksort() -Sort Array (Ascending Order), According to Key - ksort() 
+$ageNs = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+ksort($ageNs);
+print_r($ageNs);
 echo "<br>";
 
 
