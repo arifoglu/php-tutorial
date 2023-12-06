859355c935a9453468ffd637cd7ac20b96d7e979
arifoglu <88225121+arifoglu@users.noreply.github.com>
2023-12-06 14:24:08 +0100

    form validation finished

--- php-Docker-tutorial2/app/public/add.php
+++ php-Docker-tutorial2/app/public/add.php
@@ -20,13 +20,17 @@
 // <script>window.location ="https://www.bbc.co.uk"</script> 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
+$email = $title = $ingredients = "";
+
+$errors = array('email'=> '','title'=>'','ingredients'=>'');
+
+
  if(isset($_POST['submit'])){
    
-
     //check email
    if(empty($_POST['email']))
    {
-    echo "an email is required <br>";
+    $errors ['email'] = "An email is required <br>";
    }
    else
    {
@@ -34,14 +38,15 @@
     $email =$_POST['email'];
     if(!filter_var($email,FILTER_VALIDATE_EMAIL))
     {
-        echo "EMAIL must be valid";
+       // echo "EMAIL must be valid";
+    $errors ['email'] = "EMAIL must be valid";
     } 
    }
 
    //check title
    if(empty($_POST['title']))
    {
-    echo "a title is requiered <br>";
+    $errors ['title'] = "A title is requiered <br>";
    }
    else
    {
@@ -49,14 +54,16 @@
    $title = $_POST['title'];
    if(!preg_match('/^[a-zA-Z\s]+$/',$title))
    {
-    echo "TITLE must be letters ";
+   // echo "TITLE must be letters ";
+    $errors ['title'] = "TITLE must be valid";
+
    }
    }
 
    //check ingredients
    if(empty($_POST['ingredients']))
    {
-    echo "an ingredient is requiered <br>";
+    $errors ['ingredients'] = "An ingredient is requiered <br>";
    }
    else
    {
@@ -64,10 +71,23 @@
     $ingredients = $_POST['ingredients'];
     if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/',$ingredients))
     {
-     echo "Ingredients must be a comma ";
+    // echo "Ingredients must be a comma ";
+     $errors ['ingredients '] = "Ingredients must be a comma ";
+    
     } 
    }
-   };
+
+   if(array_filter($errors))
+   {
+    // echo "error in the form";
+   }
+   else
+   {
+    //echo "form is valid";
+    header('Location: index.php');
+   }
+
+};
 
  
 ?>
@@ -79,13 +99,21 @@
 
 <section class="container grey-text">
     <h4 class="center">Add a Pizza</h4>
+
     <form class="white" action="add.php" method="POST">
+
         <label>Your Email:</label>
-        <input type="text" name="email">
+        <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
+        <div class="red-text"><?php echo $errors['email'] ; ?></div>
+
         <label>Pizza Title:</label>
-        <input type="text" name="title">
+        <input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
+        <div class="red-text"><?php echo $errors['title'] ; ?></div>
+
         <label>Ingredients (comma separated):</label>
-        <input type="text" name="ingredients">
+        <input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients) ?>">
+        <div class="red-text"><?php echo $errors['ingredients'] ; ?></div>
+
         <div class="center">
             <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
         </div>
