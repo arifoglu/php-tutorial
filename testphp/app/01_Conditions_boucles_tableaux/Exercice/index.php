f46b8dc19cc76e71a485cf9668b94adb68e308a0
arifoglu <88225121+arifoglu@users.noreply.github.com>
2023-12-07 15:00:03 +0100

    php exercice has done

--- testphp/app/01_Conditions_boucles_tableaux/Exercice/index.php
+++ testphp/app/01_Conditions_boucles_tableaux/Exercice/index.php
@@ -1,13 +1,30 @@
 <?php
 $days = [ 'Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi' ];
+//echo '<pre>', var_dump( $days ), '</pre>';
+//echo '<pre>', var_dump( $days[ $theday ] ), '</pre>';
 
+///
+// 1.option
+// if(isset($_GET['day']))
+// {
+//     echo $_GET['day'];
+// }
+// else
+// {
+//     echo 0;
+// }
+
+// 2.option 
 $theday = ( isset( $_GET['day'] ) ) ? $_GET['day'] : 0;
 
 $thepage = ( isset( $_GET['page'] ) ) ? $_GET['page'] : 'foreach';
 
-echo '<pre>', var_dump( $days ), '</pre>';
-echo '<pre>', var_dump( $days[ $theday ] ), '</pre>';
-echo '<pre>', var_dump( $thepage ), '</pre>';
+//$thepageFor = ( isset( $_GET['for'] ) ) ? $_GET['for'] : 'for';
+
+//$thepageWhile = ( isset( $_GET['while'] ) ) ? $_GET['while'] : 'while';
+
+
+// echo '<pre>', var_dump( $thepage ), '</pre>';
 ?>
 <!doctype html>
 <html>
@@ -24,10 +41,10 @@ echo '<pre>', var_dump( $thepage ), '</pre>';
     <main>
         <aside>
         <form method="get">
-            <input type="text" placeholder="Recherche d'un jour" name="day" value="
-            <?php echo $theday; ?>">
-            <input type="hidden" name="page" value="for">
-            <input type="submit" value="OK">
+            <input type="text" placeholder="Recherche d'un jour" name="day" 
+            value="<?php echo $theday ; ?>">
+            <input type="hidden" name="page" value="for">                                      
+            <input type="submit" name="OK" value="OK">
         </form>
         <ul class="menu">
             <li><a class="actif" href="index.php">Foreach</a></li>
@@ -35,68 +52,74 @@ echo '<pre>', var_dump( $thepage ), '</pre>';
             <li><a class="" href="index.php">While</a></li>
         </ul>
         </aside>
-
         <section>
+           <?php 
+               switch( $thepage ){
+                   case 'foreach' : 
+                   ?>
+                         <h2>La boucle foreach</h2>
+                         <h3>Table HTML</h3>
+                         <table cellpadding="0" cellspacing="0" class="calendrier" width="100%">
+                             <?php foreach( $days as $d => $day ){ ?>
+                                 <tr>
+                                     <th>
+                                         <a class="actif" href="index.php?day=<?php echo $d; ?>">
+                                             <?php echo $day; ?>
+                                         </a>
+                                     </th>
+                                 </tr>
+                             <?php } ?>
+                         </table>
+                   <?php
+                   break;
+   
+                   case 'for' :
+                   ?>
+                         <h2>La boucle for</h2>
+                         <h3>Liste HTML</h3>
+                         <ul class="calendrier">
+                            <?php for($i = 0 ;$i < count($days);$i++){ ?>                        
+                                <li>                       
+                                       <a class="" href="index.php?day=<?php echo $days[$i];?>">  
+                                        <?php echo $days[$i]."<br>";?>
+                                       </a>
+                                </li>
+                            <?php }?>
+                        </ul>
+                   <?php
+                   break;
+   
+                   case 'while' :
+                   ?>
+                          <h2>La boucle while</h2>
+                          <h3>Paragraphe HTML</h3>
 
-
-        <?php 
-        switch( $thepage ){
-            case 'foreach' :
-                ?>
-                <h2>La boucle foreach</h2>
-                <h3>Table HTML</h3>
-                <table cellpadding="0" cellspacing="0" class="calendrier" width="100%">
-                        <?php foreach( $days as $d => $day ){ ?>
-                            <tr>
-                                <th>
-                                    <a class="actif" href="index.php?day=<?php echo $d; ?>">
-                                        <?php echo $day; ?>
-                                    </a>
-                                </th>
-                            </tr>
-                        <?php } ?>
-                </table>
-                <?php
-            break;
-
-
-            case 'for' :
-                ?>
-                <h2>La boucle for</h2>
-                <h3>Liste HTML</h3>
-                <ul class="calendrier">
-                        <li>
-                            <a class="" href="index.php">
-                                Jour
-                            </a>
-                        </li>
-                </ul>
-                <?php
-            break;
-
-
-            case 'while' :
-                ?>
-                <h2>La boucle while</h2>
-                <h3>Paragraphe HTML</h3>
-                <p class="calendrier">
-                    <a class="" href="index.php">
-                        Jour
-                    </a>
-                </p>
-                <?php
-            break;
-        }
-        ?>
-
+                          <?php while($i < count($days)){?> 
+                            <p class="calendrier">
+                                 <a class="" href="index.php?day=<?php echo $days[$i];?>">                      
+                                     <?php  echo $days[$i]."<br>";
+                                             $i++;  
+                                     ?> 
+                                 </a>
+                            </p>                                                      
+                          <?php  } ?>                                
+                           
+                   <?php
+                   break;
+               }
+           ?>
         </section>
-
 	</main>
-</div>
-    
+</div>  
 <footer>
 	Les bases du PHP
-</footer>
-    
+</footer>  
 </body>
+
 </html>
+                                <!-- <li>
+                                   <a class="" href="index.php">
+                                       Jour 
+                                   </a>
+                               </li> -->
+                               
