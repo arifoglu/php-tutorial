c0654ae68ba0d5296e219263d4bceec9fc3c303d
arifoglu <88225121+arifoglu@users.noreply.github.com>
2023-12-19 11:59:58 +0100

    i added new line to  mysqli

--- php-Docker-tutorial3/app/index.php
+++ php-Docker-tutorial3/app/index.php
@@ -10,8 +10,30 @@ if(!$conn){
     echo 'connection error :'. mysqli_connect_error();
 }
 
+// write query for all pizzas
+
+$sql = "SELECT  title,ingredients,id FROM pizzatest" ;
+
+// make query & get result
+$result = mysqli_query($conn, $sql);
+
+// fetch the resulting rows as an array
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// 
+mysqli_free_result($result);
+
+// close connection 
+mysqli_close($conn);
+
+print_r($pizzas);
+
 
 ?>
+
+
+
+
 
 <!DOCTYPE html>
 <html lang="en">
